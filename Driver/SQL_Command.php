9d9c5d0d094ac7fd76938c6129b92fb4c48a1471
Enimiste <nouni.elbachir@gmail.com>
2014-02-24 14:54:54 +0000

    Add SQL_Column_Type and implements the SQL_Update_Command_builder class
    MySql_NoQuery_Result

--- Driver/SQL_Command.php
+++ Driver/SQL_Command.php
@@ -23,8 +23,17 @@ abstract class SQL_Command_Builder {
     abstract function get();
 
     abstract function get_command_type();
+    
+    abstract function clear();
 }
 
+final class SQL_Column_Type{
+    static $INTEGER = 1;
+    static $STRING = 2;
+    static $DATE = 3;
+    static $FLOAT = 4;
+    static $BOOLEAN = 5;
+}
 abstract class SQL_Select_Command_Builder extends SQL_Command_Builder {
 
     abstract function select($table_name, $field_name, $alias = FALSE);
@@ -34,14 +43,18 @@ abstract class SQL_Select_Command_Builder extends SQL_Command_Builder {
     abstract function join($right_table_name, $right_field_name, $op, $left_table_name, $left_field_name, $join_type = "left");
 
     abstract function where($table_name, $field_name, $op, $value);
-
+    
     abstract function limit($limit);
 
     abstract function order_by($table_name, $field_name, $order = "ASC");
 }
 
 abstract class SQL_Update_Command_builder extends SQL_Command_Builder {
+    abstract function update_table($table_name);
     
+    abstract function column($field_name, $field_new_value, $type = SQL_Column_Type::STRING);
+    
+    abstract function where($table_name, $field_name, $op, $value);
 }
 
 abstract class SQL_Command_Executor {
@@ -51,7 +64,7 @@ abstract class SQL_Command_Executor {
         return $this;
     }
 
-    abstract function execute_query(Mysql_Command_Builder $command);
+    abstract function execute_query(SQL_Command_Builder $command);
 
     abstract protected function save_server_param(Common_Server_Param $params);
 }
@@ -61,7 +74,8 @@ abstract class SQL_Result_Set implements Iterator {
 }
 
 abstract class SQL_NoQuery_Result {
-    
+    abstract function get_bool_value();
+    abstract function get_int_value();
 }
 
 
--- Mysql/MySQL_Command.php
+++ Mysql/MySQL_Command.php
@@ -4,9 +4,9 @@ $driver = realpath(__DIR__ . "/../" . "Driver/SQL_Command.php");
 require_once $driver;
 
 /**
-*
-* @author NOUNI EL BACHIR
-*/
+ *
+ * @author NOUNI EL BACHIR
+ */
 class Mysql_Select_Command_Builder extends SQL_Select_Command_Builder {
 
     private $select_fields;
@@ -14,20 +14,13 @@ class Mysql_Select_Command_Builder extends SQL_Select_Command_Builder {
     private $joins;
     private $wheres;
     private $order_by;
-    private $command_type;
+    private $limit;
 
     function __construct() {
-        $this->table_prefix = "";
-        $this->select_fields = array();
-        $this->from = "";
-        $this->joins = array();
-        $this->wheres = array();
-        $this->order_by = "";
-        $this->command_type = FALSE;
+        $this->clear();
     }
-
+    
     function select($table_name, $field_name, $alias = FALSE) {
-        $this->command_type = SQL_Command_Builder::$SELECT_COMMAND;
         $table_name = $this->table_prefix . $table_name;
         if ($alias)
             $field_name = $field_name . " AS " . $alias;
@@ -63,12 +56,6 @@ class Mysql_Select_Command_Builder extends SQL_Select_Command_Builder {
         return $this;
     }
 
-    public function limit($limit) {
-        if(!is_numeric($limit))
-            throw new SQL_Exception("The limit should be an integer value.");
-        $this->limit = $limit;
-    }
-
     public function get() {
         $sql = "SELECT " . implode(", ", $this->select_fields) . " ";
         $sql .= " FROM " . $this->from . " ";
@@ -84,7 +71,25 @@ class Mysql_Select_Command_Builder extends SQL_Select_Command_Builder {
     }
 
     public function get_command_type() {
-        return $this->command_type;
+        return parent::$SELECT_COMMAND;
+    }
+
+    public function limit($limit) {
+        if (!is_numeric($limit))
+            throw new SQL_Exception("The limit should be an integer value.");
+        $this->limit = $limit;
+        
+        return $this;
+    }
+
+    public function clear() {
+        $this->table_prefix = "";
+        $this->select_fields = array();
+        $this->from = "";
+        $this->joins = array();
+        $this->wheres = array();
+        $this->order_by = "";
+        $this->limit = FALSE;
     }
 
 }
@@ -99,14 +104,19 @@ class Mysql_Command_Executor extends SQL_Command_Executor {
         $this->params = $params;
     }
 
-    public function execute_query(Mysql_Command_Builder $command) {
-        mysql_connect($this->params->getHost_name(), $this->params->getUser_name(), $this->params->getPassword()) or die(mysql_error());
+    public function execute_query(SQL_Command_Builder $command) {
+        $link_mysql = mysql_connect($this->params->getHost_name(), $this->params->getUser_name(), $this->params->getPassword()) or die(mysql_error());
         mysql_select_db($this->params->getDb_name()) or die(mysql_error());
-        $result = mysql_query($command->get()) or die(mysql_error());
-        if ($command->get_command_type() == SQL_Command_Builder::$SELECT_COMMAND)
-            return new Mysql_Result_Set($result);
-        else
-            NULL;
+        
+        if ($command->get_command_type() == SQL_Command_Builder::$SELECT_COMMAND){
+            $result = mysql_query($command->get()) or die(mysql_error());
+            $res = new Mysql_Result_Set($result);
+        } else {
+            $result = mysql_query($command->get()) or die(mysql_error());
+            $res = new MySql_NoQuery_Result(mysql_affected_rows());
+        }
+        mysql_close($link_mysql);
+        return $res;
     }
 
 }
@@ -147,9 +157,10 @@ class Mysql_Result_Set extends SQL_Result_Set {
         return $this->current;
     }
 
-    function first_row(){
+    function first_row() {
         $this->rewind();
-        if($this->valid()) return $this->current;
+        if ($this->valid())
+            return $this->current;
         throw new SQL_Exception("The result set is empty.");
     }
 
@@ -157,18 +168,113 @@ class Mysql_Result_Set extends SQL_Result_Set {
 
 class Mysql_Update_Command_Builder extends SQL_Update_Command_builder {
 
+    private $table_name;
+    private $columns;
+    private $wheres;
+
+    function __construct() {
+        $this->clear();
+    }
+
+    public function update_table($table_name) {
+        $this->table_name = $this->table_prefix . $table_name;
+
+        return $this;
+    }
+
+    public function column($field_name, $field_new_value, $type = SQL_Column_Type::STRING) {
+        if (array_key_exists($field_name, $this->columns))
+            throw new SQL_Exception("Column specified in the update command already exist.");
+        $this->valdiate_type($field_new_value, $type);
+        $this->columns[$field_name] = $field_new_value;
+
+        return $this;
+    }
+
+    public function where($table_name, $field_name, $op, $value) {
+        $table_name = $this->table_prefix . $table_name;
+        $this->wheres[] = $table_name . "." . $field_name . " " . $op . " " . $value;
+
+        return $this;
+    }
+
     public function get() {
-        
+        $sql = "UPDATE " . $this->table_name . " SET ";
+        $sql .= implode(", ", array_map(function($key, $item) {
+                            return $key . "=" . $item;
+                        }, array_keys($this->columns), array_values($this->columns)));
+        if (!empty($this->wheres))
+            $sql .= " WHERE " . implode(" AND ", $this->wheres) . " ";
+        return $sql;
     }
 
     public function get_command_type() {
-        
+        return parent::$Update_COMMAND;
+    }
+
+    //***************** Private functions
+    private function valdiate_type($value, $type) {
+        switch ($type) {
+            case SQL_Column_Type::$INTEGER:
+                if (!is_numeric($value))
+                    throw new Exception("Invalid value in a column of type INTEGER.");
+                try {
+                    $value = intval($value);
+                } catch (Exception $ex) {
+                    throw new Exception("Invalid value in a column of type INTEGER.");
+                }
+                break;
+            case SQL_Column_Type::$FLOAT:
+                if (!is_numeric($value))
+                    throw new Exception("Invalid value in a column of type FLOAT.");
+
+                break;
+            case SQL_Column_Type::$DATE:
+                if (!is_a($value, "DateTime"))
+                    try {
+                        $value = date_create_from_format($value);
+                    } catch (Exception $ex) {
+                        throw new Exception("Invalid value in a column of type INTEGER.");
+                    }
+
+                break;
+            case SQL_Column_Type::$STRING:
+                if (!is_string($value))
+                    throw new Exception("Invalid value in a column of type STRING.");
+
+                break;
+            case SQL_Column_Type::$BOOLEAN:
+                if (!is_bool($value))
+                    throw new Exception("Invalid value in a column of type BOOLEAN.");
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    public function clear() {
+        $this->table_name = "";
+        $this->columns = array();
+        $this->wheres = array();
     }
 
 }
 
 class MySql_NoQuery_Result extends SQL_NoQuery_Result {
+    private $result;
     
+    function __construct($result) {
+        $this->result = $result;
+    }
+
+    public function get_bool_value() {
+        return $this->result;
+    }
+
+    public function get_int_value() {
+        if(is_numeric($this->result)) return intval($this->result);
+    }    
 }
 
 ?>
